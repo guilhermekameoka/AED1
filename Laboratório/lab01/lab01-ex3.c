4d965c9ea3e0eedcec169b491abd74d992630f72
Guilherme R. Kameoka <80076658+guilhermekameoka@users.noreply.github.com>
2022-05-10 23:06:33 -0300

    Add files via upload

--- Laboratório/lab01/lab01-ex3.c
+++ Laboratório/lab01/lab01-ex3.c
@@ -6,14 +6,14 @@ int main()
 
     int bit[4], decimal;
 
-    for (int i = 1; i < 5; i++)
+    for (int i = 0; i < 4; i++)
     {
 
-        printf("digite o %do bit: ", i);
+        printf("digite o %do bit: ", i+1);
         scanf("%d", &bit[i]);
     }
 
-    decimal = pow((bit[4] * 2), 0) + pow((bit[3] * 2), 1) + pow((bit[2] * 2), 2) + pow((bit[1] * 2), 3);
+    decimal = pow((bit[3] * 2), 0) + pow((bit[2] * 2), 1) + pow((bit[11] * 2), 2) + pow((bit[0] * 2), 3);
 
-    printf("O numero binario %d%d%d%d corresponde ao numero decimal %d", bit[1], bit[2], bit[3], bit[4], decimal);
+    printf("O numero binario %d%d%d%d corresponde ao numero decimal %d", bit[0], bit[1], bit[2], bit[3], decimal);
 }
