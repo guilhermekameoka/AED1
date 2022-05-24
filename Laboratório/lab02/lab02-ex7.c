c867d1d2542e194f60ba2c94df6fc9a6bde71214
Guilherme R. Kameoka <grkame@ufu.br>
2022-05-24 00:26:40 -0300

    ex7 att

--- Laboratório/lab02/lab02-ex7.c
+++ Laboratório/lab02/lab02-ex7.c
@@ -5,7 +5,7 @@ typedef struct ponto
 {
 
     int x, y;
-    
+
 } ponto;
 
 int main()
@@ -16,15 +16,23 @@ int main()
     printf("Quantos pontos deseja digitar: ");
     scanf("%d", &n);
 
-    v = (int *)malloc(n * sizeof(int));
+    v = (int *)malloc(n * sizeof(ponto));
 
-    ponto v[n];
+    ponto vet[n];
+    v = vet;
 
     for (int i = 0; i < n; i++)
     {
         printf("Entre com a coordenada x do ponto %d: ", i + 1);
-        scanf("%d", &v.x[i]);
+        scanf("%d", &vet[i].x);
         printf("Entre com a coordenada y do ponto %d: ", i + 1);
-        scanf("%d", &v.y[i]);
+        scanf("%d", &vet[i].y);
+    }
+
+    printf("Pontos digitados: ");
+
+    for (int i = 0; i < n; i++)
+    {
+        i < n ? printf("(%d,%d);", vet[i].x, vet[i].y) : printf("(%d,%d)", vet[i].x, vet[i].y);
     }
 }
