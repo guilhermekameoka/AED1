a35b534c5ef20a55d0e1d6c38c3ec65cf5738096
Guilherme R. Kameoka <80076658+guilhermekameoka@users.noreply.github.com>
2022-07-11 00:42:34 -0300

    correcao remove_elemento()

--- Laboratório/lab08/ex3/lista_din_duplamente_encad_ord.c
+++ Laboratório/lab08/ex3/lista_din_duplamente_encad_ord.c
@@ -62,33 +62,26 @@ int insere_elemento(Lista *lst, int elem)
 
 int remove_elemento(Lista *lst, int elem)
 {
-    if (lista_vazia(*lst))
-    {
+    if (lista_vazia(*lst) == 1)
         return 0;
-    }
 
-    Lista aux = *lst; // Faz aux apontar para o primeiro no
+    Lista aux = *lst;
+
     while (aux->prox != NULL && aux->info != elem)
-    {
         aux = aux->prox;
-    }
 
-    if (aux->info != elem)
-    {
-        return 0;
-    }
+    if (aux->info != elem || aux->prox == NULL || aux->prox->info > elem) // trata o final da lista
+        return 0;                                                         // falha
+
     if (aux->prox != NULL)
-    {
         (aux)->prox->ant = aux->ant;
-    }
+
     if (aux->ant != NULL)
-    {
         (aux)->ant->prox = aux->prox;
-    }
+
     if (aux == *lst)
-    {
         *lst = aux->prox;
-    }
+
     free(aux);
     return 1;
 }
